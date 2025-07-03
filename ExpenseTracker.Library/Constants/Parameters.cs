6e59ffe94bb8a3e6c8121e11cd16c444f8e9422c
zhenkaiwong <zhenkaiwong.work@gmail.com>
2025-07-03 23:57:26 +0800

    Some constants value for reusability

---
+++ ExpenseTracker.Library/Constants/Parameters.cs
@@ -0,0 +1,13 @@
+using ExpenseTracker.Library.Utils;
+
+namespace ExpenseTracker.Library;
+
+public partial class Constants
+{
+    public class Parameters
+    {
+        public const string DESCRIPTION = "--description";
+        public const string AMOUNT = "--amount";
+        public const string MONTH = "--month";
+    }
+}
