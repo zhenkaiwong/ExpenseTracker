41c465c31785c59ab67f127ca7b284f1578cb35d
zhenkaiwong <zhenkaiwong.work@gmail.com>
2025-07-25 21:45:22 +0800

    update CanExecute() to use validator

--- ExpenseTracker.Library/Commands/ListCommand.cs
+++ ExpenseTracker.Library/Commands/ListCommand.cs
@@ -1,6 +1,7 @@
 using ExpenseTracker.Library.Models;
 using ExpenseTracker.Library.Services;
 using ExpenseTracker.Library.Utils;
+using ExpenseTracker.Library.Validators;
 
 namespace ExpenseTracker.Library.Commands;
 
@@ -8,9 +9,9 @@ public class ListCommand : BaseCommand
 {
     protected override bool CanExecute(UserInput userInput)
     {
-        // this command can always be executed regardless user input
-        // since it doesn't rely on any data from it
-        return true;
+        EmptyUserInputValidator validator = new();
+        ValidationResult result = validator.Validate(userInput);
+        return result.Success;
     }
 
     protected override void DoExecute(UserInput userInput)
